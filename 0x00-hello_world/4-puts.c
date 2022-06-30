0c66991b7fd2a55d1e84822bd18f2562bba7784f
Emezi Uchechi David <daviduciboy@gmail.com>
2022-06-30 05:45:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*
+ *
+ * Entry point: main
+ * 
+ * Return always (0): Success
+ *
+ */
+int main(void)
+{ 
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
