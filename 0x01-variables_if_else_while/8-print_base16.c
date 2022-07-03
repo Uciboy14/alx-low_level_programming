c6675a95428d5179254ae712be8f580808cff392
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-03 15:54:03 +0100

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,10 @@ int main(void)
 {
 	int n;
 	char ch;
- for (n = 48; n < 58; n++)
-	 putchar(n);
- for (ch = 'a'; ch <= 'f'; ch++)
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
