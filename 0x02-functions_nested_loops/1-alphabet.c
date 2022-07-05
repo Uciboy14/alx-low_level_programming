9b218368f513c506aad600a40c2dc5917726d074
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-05 13:51:05 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,29 @@
 #include "main.h"
 
 /**
+ * print_alphabet - print alphabet, followed by
+ * newline
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ch;
+        ch = 'a';
+
+        while (ch <= 'z')
+        {
+                _putchar(ch);
+                ch++;
+        }
+        _putchar('\n');
+}
+
+/**
  * main - check the code
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
-	ch = 'a';
-	
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
 }
-
