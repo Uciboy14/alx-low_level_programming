aeffaaa0bbcc3c0e1965c5b37cfd4d087d726f62
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-06 12:35:49 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting 
+ * times_table - prints the 9 times table, starting
  */
 void times_table(void)
 {
-	int i, j, k;
+	int i,j,k;
 
 	for (i = 0; i < 10; i++)
 	{
