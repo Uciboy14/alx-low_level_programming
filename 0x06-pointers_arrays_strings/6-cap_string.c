74c1a53281e0940944ca0cc45b314fdc3d4c225b
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-14 23:58:29 -0700

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
-#include "mai.h"
-/*	
+#include "main.h"
+/**
 * cap_string - Capitalizes all words of a string.
 * @str: The string to be capitalized.
 *
@@ -10,11 +10,24 @@ char *cap_string(char *str)
 	int index = 0;
 
 	while (str[index])
-	{ 
+	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-	      1      		index++;
-		if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}' || index == 0) str[index] -= 32;
-	index++;
+			index++;
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' || index == 0)
+			str[index] -= 32;
+		index++;
 	}
 	return (str);
 }
