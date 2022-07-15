7432147a65be8cefbb3040adcfdcd1c6a6880012
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-15 11:58:18 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - all possiblee combinations of single-digit
+ * Return: Always Success (0)
+ */
+int main (void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+
+}
