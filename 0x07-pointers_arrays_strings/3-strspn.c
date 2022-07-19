cf91613c414ccf10c94af320ae3617ea3b9e2f5b
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-19 14:46:54 +0100

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,13 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i,j, f, flag;
-
+	
 	f = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
+
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
@@ -30,7 +31,5 @@ unsigned int _strspn(char *s, char *accept)
 			return (f);
 		}
 	}
-
 	return (0);
 }
-
