442e85ede1586933dab7ba16d8a91c05df541fc3
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-19 10:46:10 +0100

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char );
 char *_memset (char *, char, unsigned int n);
 char *_memcpy (char *, char *, unsigned int n);
 char *_strchr (char *, char);
@@ -9,5 +10,6 @@ char *_strpbrk (char *, char *);
 char *_strstr (char *, char *);
 void print_chessboard (char (*a)[8]);
 void print_diagsums (int *, int);
+
 
 #endif
