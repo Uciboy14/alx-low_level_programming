7017d09be0eb1b59b969d7d3eae2dda8f2831819
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-19 14:55:05 +0100

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,13 +5,13 @@
  * @s: string to evaluatee
  * @accept: string containing the list of characters
  *
- * Return: the number of byte in the initial segment 
+ * Return: the number of byte in the initial segment
  * of s which consist only of bytes from accept.
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, f, flag;
-	
+
 	f = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
