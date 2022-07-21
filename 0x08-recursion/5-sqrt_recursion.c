b9425b9567c1ca8965df7ca65dad15a7fb6c9d4b
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-21 06:05:35 +0100

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,14 +13,14 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return ( actual_sqrt_recursion(n, 0));
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
  * actual_sqrt_recursion - recurses to find the
  * natural square root of a number
  * @n: number to calculate the square root of
- * i: iteration
+ * @i: iteration
  *
  * Return: the resulting square root
  */
