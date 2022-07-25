22401fba8fcfd532c69e716e4ad2e3784914cd78
Emezi Uchechi David <daviduciboy@gmail.com>
2022-07-25 16:49:32 +0100

    update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - mutiplies to numbers
+ * @argc: argument count
+ * @argv: argumemt array
+ *
+ * Return: 0 if no Error, otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+	
+	/* if the input isn't two intgers */
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		/* if the input isn't numbers */
+		if ( (a >= 0 || a <= 9) && (b >= 0 || b <= 9))
+		{
+			a  = _atoi(argv[1]);
+			b  = _atoi(argv[2]);
+			c  = a * b;
+			printf("%d\n", c);
+		}
+
+		return (0);
+	}
+}
+
+	
---
+++ 0x0A-argc_argv/main.c
@@ -0,0 +1,13 @@
+#include  "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count                               * @argv: array of arguments
+ *
+ * Return: 0 for success, otherwise 1
+ */
+int main(void)
+{
+        printf("%d\n", _atoi("1002"));
+        printf("%d\n", _atoi("1003"));
+                                                              return (0);                                   }
