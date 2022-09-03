0c79399c3ddcf3a28fbf9c998d582028798c3d81
Emezi Uchechi David <daviduciboy>
2022-09-03 07:17:49 +0100

    update

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
  * add_nodeint - add a new node at the beginning of a
  * linked list
  * @head: pointer to the first node in the list
- * @a: data to insert in that new node
+ * @n: data to insert in that new node
  *
  * Return: pointer to the new node, or NULL if it
  * fails
